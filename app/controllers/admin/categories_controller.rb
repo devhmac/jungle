32e28a13af362344cfe0856c45450c66e2df2088
Devin MacGillivray <devhmac@gmail.com>
2021-04-29 00:45:29 +0000

    changed product_params to category_params

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -10,7 +10,7 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
-    @category = Category.new(product_params)
+    @category = Category.new(category_params)
 
     if @category.save
       redirect_to [:admin, :categories], notice: 'Category created!'
@@ -19,7 +19,7 @@ class Admin::CategoriesController < ApplicationController
     end
   end
 
-  def product_params
+  def category_params
     params.require(:category).permit(
       :name
     )
