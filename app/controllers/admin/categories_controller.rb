81fd6d6017d85f03f55efa3b7cf8871f4927fef5
Devin MacGillivray <devhmac@gmail.com>
2021-04-29 00:31:42 +0000

    building new categories form

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -9,5 +9,19 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new
   end
 
+  def create
+    @category = Category.new(product_params)
 
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  def product_params
+    params.require(:category).permit(
+      :name
+    )
+  end
 end
