a38857e398f2d803e4aff58e076f5b8e5baea0f6
Devin MacGillivray <devhmac@gmail.com>
2021-05-03 21:51:07 +0000

    adds more user validation and validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :name, presence: true
+  validates :last_name, presence: true
   validates :email, uniqueness: { case_sensitive: false }, presence: true
-  validates :password, presence: true
+  validates :password, length: {minimum: 3}, presence: true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+   
+    it 'is valid with valid attributes' do
+      
+       params = {
+       name:'bob',
+       last_name:'bobby',
+       email: 'dev@dev',
+       password:'123',
+       password_confirmation:'123'
+       }
+       expect(User.new(params)).to be_valid
+
+     end
+    it 'should not be valid if password is less than 3 characters' do
+       params = {
+       name:'bob',
+       last_name:'bobby',
+       email: 'dev@dev',
+       password:'12',
+       password_confirmation:'12'
+       }
+       expect(User.new(params)).to_not be_valid
+     end
+    it 'should not be valid if password and pass confirmation do not match' do
+       params = {
+       name:'bob',
+       last_name:'bobby',
+       email: 'dev@dev',
+       password:'123',
+       password_confirmation:'1233'
+       }
+       expect(User.new(params)).to_not be_valid
+     end
+ 
+   
+    
+    end
 end
