62a6f8d9a49f0879efe8fe65d1c94a6da4717b24
Devin MacGillivray <devhmac@gmail.com>
2021-05-04 19:35:49 +0000

    adds test for successful navigation to item details page after header click

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  #setup 
+  before :each do 
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see all products" do
+    # ACT
+    visit root_path
+
+    # DEBUG / VERIFY
+   first('.product > header').click
+   expect(page).to have_css 'section.products-show'
+    save_screenshot 'product_details_test.png'
+  end
+end
