67c76be88f14d90d2920b16e8811fa6ae2f3c3a2
Devin MacGillivray <devhmac@gmail.com>
2021-05-04 19:44:51 +0000

    changes test description to fit

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -16,7 +16,7 @@ RSpec.feature "ProductDetails", type: :feature, js: true do
     end
   end
 
-  scenario "They see all products" do
+  scenario "They see product details when clicking on an individual product" do
     # ACT
     visit root_path
 
